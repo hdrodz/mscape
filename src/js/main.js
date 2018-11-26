83f95cc453f3f2b903f365ef39d3cb26e390d7d8
hdrodz97 <hdrodz97@users.noreply.github.com>
2018-11-26 18:53:38 -0500

    add scripts to the main page to begin debugging

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+//
+// main.js -- Application entry point
+//
+
+window.onload = () => {
+    const canvas = document.getElementById("canvas");
+    tryInitWebGL(canvas);
+
+    const testLayer = new RenderLayer();
+    testLayer.render = (now) => {
+        gl.clearColor(0, 0.5, 1, 1);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+    };
+
+    const renderer = new Renderer(19280, 1080, 
+        canvas.clientWidth, canvas.clientHeight,
+        [testLayer], [BlendMode.NORMAL]);
+    
+    requestAnimationFrame(renderer.render);
+}
